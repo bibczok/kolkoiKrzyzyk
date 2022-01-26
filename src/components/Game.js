29ecf57ab98a51ba8bfd00246566a47cd4ba5281
Very Average Programista <94396142+cieksu@users.noreply.github.com>
2022-01-26 06:00:14 +0100

    Znaczne zmiany w Game,js

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,35 +8,50 @@ export default class Game extends Component {
         this.state = {
             xIsNext: true,
             stepNumber: 0,
+            reversedHistory: false,
             history: [
-                { squares: Array(9).fill(null) }
-            ]
-        }
+                { squares: Array(9).fill(null), 
+                  position: null  } /*opisujemy poszczegolne komponenty
+                   i nadajemy im stany*/
+            ],
+        };
+
     }
 
     jumpTo(step){
         this.setState({
             stepNumber: step,
             xIsNext: (step%2)===0
-        })
+        }); /*metoda ta aktualizuje wartosc stepNumber
+        wraz z przebiegiem rozgrywki*/
+    }
+    
+    reversedHistory() {
+        this.setState({
+            reversedHistory: !this.state.reversedHistory,
+        }); /*funkcja obracania historii ruchów*/
     }
 
     handleClick(i){
         const history = this.state.history.slice(0, this.state.stepNumber+1);
         const current = history[history.length-1];
         const squares = current.squares.slice();
+        const position = getPosition(i);
         const winner = Winner(squares);
         if(winner || squares[i]){
             return;
-        }
+        }/*dzięki handleClick mozemy przechowywać wszystkie ważne
+        informacje w jednym miejscu*/
+        
         squares[i] = this.state.xIsNext?'X':'O'
         this.setState({
             history: history.concat({
-                squares: squares
+                squares,
+                position,
             }),
             xIsNext: !this.state.xIsNext,
             stepNumber: history.length
-        })
+        }) /*program sprawdza czy teraz jest tura X czy O*/
     }
 
     render() {
@@ -45,36 +60,50 @@ export default class Game extends Component {
         const current = history[this.state.stepNumber];
         const winner = Winner(current.squares);
         const moves = history.map((step, move)=>{
-            const desc = move? 'Cofnij do '+ move +' ruchu':'Zacznij grac';
+             const desc = move? 'Cofnij do '+ move +' ruchu ' + `( ${history[move].position.join('')} )` :'Zacznij grac';
+            const bold = move === this.state.stepNumber ?'bold':'';
             return (
-                <li key={move}>
+                <li key={move}  className={bold}>
                     <button onClick={()=>{this.jumpTo(move)}}>
                         {desc}
                     </button>
                 </li>
             )
-        });
+        }); /*metoda render w tym przypadku odczytuje aktualny status gry
+            oraz aktualizuje stepNumber o wykonane ruchy, zapisuje je
+            nastepnie na mapie. jumpTo pozwala nam wrocic do wczesniejszych ruchow*/
 
-        let status;
+        let movesView=this.state.reversedHistory?moves.reverse():moves;
+
+         let status;
         if(winner){
-            status = 'Wygrywa zawodnik ' + winner;
+            status = 'Wygrywa zawodnik ' + winner.name;
+        }else if(this.state.stepNumber===9) {
+            status = 'Remis';
         }else{
             status = 'Nastepny ruch zawodnika '+ (this.state.xIsNext?'X':'O');
-        }
+        } /*okres warunkowy sprawdzający który decyduje o tym 
+        czy gra jest kontynułowana dalej czy też jest zakończona*/
 
 
-        return (
+ return (
             <div className="game">
                 <div className="gameBoard">
                     <Board onClick={(i)=>this.handleClick(i)}
-                    squares={current.squares} />
+                    squares={current.squares} 
+                    winner = {winner} /> 
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
+                    <div>
+                        <button onClick={()=>this.reversedHistory()}>Odwróć historie ruchów</button> 
+                    </div>
                     <ul>{moves}</ul>
                 </div>
             </div>
-        )
+        ) /*funkcja sprawdzająca czy przypadkiem po wykonanym ruchu
+         nie nastąpiła wygrana któregośz graczy oraz przycisk do
+         odwrócenia historii ruchów*/
     }
 }
 
@@ -87,19 +116,27 @@ function Winner(squares) {
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 4, 6]
+        [2, 4, 6] /*wszystkie możliwe wygrane kombinacje
+                    które pozwolą nam na wyłonienie zwycięzcy*/
     ];
 
     for(let i=0; i<lines.length; i++){
         const [a, b, c] = lines[i];
         if(squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
-            return squares[a];
+            return { 
+            name: squares[a],
+            line: lines[i],
+            };
         }
     }
-    
-    
-    
-    
-    
     return null;
+}
+
+function getPosition(i) {
+    const rowMap = [
+        ['a',3], ['b',3], ['c',3],
+        ['a',2], ['b',2], ['c',2],
+        ['a',1], ['b',1], ['c',1],
+    ]
+    return rowMap[i]; /*zwrócenie "mapki" z koordami kazdego ruchu*/
 }
