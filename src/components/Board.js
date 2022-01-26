4743bf064860f13dd51fce25515feaf2be970a5a
Very Average Programista <94396142+cieksu@users.noreply.github.com>
2022-01-26 05:52:23 +0100

    Zmiany w Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,30 +2,46 @@ import React, { Component } from 'react';
 import Square from './Square';
 
 export default class Board extends Component {
+    
+     wonSquare(i) {
+        if(this.props.winner&&this.props.winner.line.findIndex(el=>el===i)!==-1) {
+            return true;
+        } /*sprawdzanie wygranych pól*/
+        return null;
+    }
+    
     renderSquare(i){
         return <Square value={this.props.squares[i]}
         onClick={()=>this.props.onClick(i)}
-        />
+        winner={this.wonSquare(i)}
+        key={i}
+        /> /*sprawdzanie który gracz zdobył wygrane pola*/
     }
     render() {
         return (
-            <div>
-                <div className="border-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+            <div className="board">
+                <div className="row-counters">
+                    <div className="row-num">1</div>
+                    <div className="row-num">2</div>
+                    <div className="row-num">3</div>
                 </div>
-                <div className="border-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
+                {
+                    Array(3).fill(null).map((row,x)=> {
+                        return (
+                            <div className="board-row" key={x}>
+                                {
+                                    Array(3).fill(null).map((square,y)=>this.renderSquare(3*x+y))
+                                }
+                            </div>
+                        )
+                    })
+                }
+                <div className="column-counters">
+                    <div className="column-num">a</div>
+                    <div className="column-num">b</div>
+                    <div className="column-num">c</div>
                 </div>
-                <div className="border-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        )
+            </div> /*przypisanie odpowiedniej litery do danej kolumny*/
+        );
     }
 }
